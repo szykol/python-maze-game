e0e83e8b8c776a8aa515c3c6782bf09ebe9dc19a
szykol <szymonkolton@gmail.com>
2019-04-27 16:37:32 +0200

    Add Player class

---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    RIGHT, DOWN, LEFT, UP = 0, 1, 2, 3
+    def __init__(self, center):
+        self.direction = Player.RIGHT
+        self.rect.center = center
+        pygame.sprite.Sprite.__init__(self)
+
+    @property
+    def direction(self):
+        return self.__direction
+
+    @direction.setter
+    def direction(self, direction):
+        self.__direction = direction
+        
+        self.image = pygame.image.load('img/player_{}.png'.format(direction))
+        if not hasattr(self, 'rect'):
+            self.rect = self.image.get_rect()
+
+        
+        
