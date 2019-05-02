7fdc0c35bbbd34a3dbbc2686db549a935cb75b51
szykol <szymonkolton@gmail.com>
2019-05-02 02:42:48 +0200

    Add moves algorithm

--- game.py
+++ game.py
@@ -30,9 +30,9 @@ class Game:
     def __init__(self):
         self.clock = pygame.time.Clock()
         self.done = False
-        self.screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((1920, 1080))#, pygame.FULLSCREEN)
         self.plates = spawn_simple_maze((400, 200))
-        
+
         background = pygame.sprite.Sprite()
         background.image = pygame.image.load('img/maze.png')
         background.rect = background.image.get_rect()
@@ -69,6 +69,8 @@ class Game:
     def _setup(self):
         self.player_index = (0, 1)
         self.end_index = (1,7)
+        self.forks = []
+        self.visited_forks = []
 
         if hasattr(self, 'player'):
             self.all_sprites.remove(self.player)
@@ -77,6 +79,7 @@ class Game:
         self.all_sprites.add(self.player)
 
         self._show_neighb_plates()
+        self._check_neighb_plates()
         self._enable_move()
 
     def _handle_keys(self):
@@ -109,9 +112,29 @@ class Game:
             return
 
         if self.plates[next_pos].type == Plate.YELLOW:
+            # if player_index is the newest fork index
+            if self.forks and self.player_index == self.forks[-1]['index']:
+                print(self.forks)
+                # if direction is the same as fork's current direction
+                if direction == self.forks[-1]['directions'][0]:
+                    # pop direction when the movement is correct
+                    print(self.forks[-1]['directions'])
+                    self.forks[-1]['directions'].pop(0)
+                    print(self.forks[-1]['directions'])
+
+                    # if there's no more directions - pop the current fork
+                    if not self.forks[-1]['directions']:
+                        self.forks.pop()
+                        print(self.forks)
+                else:
+                    self._setup()
+                    return
+
+            prev_pos = self.player_index
             self.player_index = next_pos
             self.player.rect.center = self.plates[self.player_index].rect.center
             self._show_neighb_plates()
+            self._check_neighb_plates(prev_pos)
             self.player.direction = direction
 
             self.can_move = False
@@ -128,6 +151,34 @@ class Game:
         for y in range(slice.shape[1]):
             for x in range(slice.shape[0]):
                 slice[x, y].visible = True
+
+    def _check_neighb_plates(self, prev_pos=None):
+        x, y = self.player_index
+
+        indices = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+        directions = []
+        for i in range(4):
+            # print(indices[i])
+            pos_x, pos_y = indices[i]
+            if pos_x > 0 and pos_x <= 7 and pos_y > 0 and pos_y <= 7:
+                if self.plates[pos_x, pos_y].type == Plate.YELLOW:
+                    if prev_pos is not None and (pos_x, pos_y) != prev_pos:
+                        directions.append(i)
+                    # print('App')
+                    # print(indices[i])
+                    # print(directions)
+
+        if len(directions) > 1 and not (x,y) in self.visited_forks:
+            self.forks.append({
+                'index': (x,y),
+                'directions': directions
+            })
+            self.visited_forks.append((x,y))
+            print('Appending')
+        else:
+            self.no_move = True
+
+
 
 
 if __name__ == "__main__":
